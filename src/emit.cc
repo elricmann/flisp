e03ba4ba0d45d75d919f300c6839bebfb9888620
elricmann <>
2024-09-01 23:53:09 +0300

    chore: write class files

--- src/emit.cc
+++ src/emit.cc
@@ -2,41 +2,41 @@
 
 #include <iostream>
 #include <memory>
-#include <string>
 // #include <unordered_map>
+#include <fstream>
 
-jvm_emitter::jvm_emitter() : bytecode_() {}
+jvm_emitter::jvm_emitter() : bytecode() {}
 
-void jvm_emitter::emit_nop() { bytecode_.push_back(OP_NOP); }
+void jvm_emitter::emit_nop() { bytecode.push_back(OP_NOP); }
 
 void jvm_emitter::emit_iconst(int value) {
   switch (value) {
     case 0:
-      bytecode_.push_back(OP_ICONST_0);
+      bytecode.push_back(OP_ICONST_0);
       break;
     case 1:
-      bytecode_.push_back(OP_ICONST_1);
+      bytecode.push_back(OP_ICONST_1);
       break;
     case 2:
-      bytecode_.push_back(OP_ICONST_2);
+      bytecode.push_back(OP_ICONST_2);
       break;
     case 3:
-      bytecode_.push_back(OP_ICONST_3);
+      bytecode.push_back(OP_ICONST_3);
       break;
     case 4:
-      bytecode_.push_back(OP_ICONST_4);
+      bytecode.push_back(OP_ICONST_4);
       break;
     case 5:
-      bytecode_.push_back(OP_ICONST_5);
+      bytecode.push_back(OP_ICONST_5);
       break;
     default:
       if (value >= -128 && value <= 127) {
-        bytecode_.push_back(OP_BIPUSH);
-        bytecode_.push_back(static_cast<uint8_t>(value));
+        bytecode.push_back(OP_BIPUSH);
+        bytecode.push_back(static_cast<uint8_t>(value));
       } else if (value >= -32768 && value <= 32767) {
-        bytecode_.push_back(OP_SIPUSH);
-        bytecode_.push_back(static_cast<uint8_t>(value >> 8));
-        bytecode_.push_back(static_cast<uint8_t>(value & 0xFF));
+        bytecode.push_back(OP_SIPUSH);
+        bytecode.push_back(static_cast<uint8_t>(value >> 8));
+        bytecode.push_back(static_cast<uint8_t>(value & 0xFF));
       } else {
         throw std::runtime_error("value out of range for iconst/bipush/sipush");
       }
@@ -45,34 +45,50 @@ void jvm_emitter::emit_iconst(int value) {
   }
 }
 
-void jvm_emitter::emit_ireturn() { bytecode_.push_back(OP_IRETURN); }
+void jvm_emitter::emit_ireturn() { bytecode.push_back(OP_IRETURN); }
 
-void jvm_emitter::emit_return() { bytecode_.push_back(OP_RETURN); }
+void jvm_emitter::emit_return() { bytecode.push_back(OP_RETURN); }
 
-void jvm_emitter::emit_iadd() { bytecode_.push_back(OP_IADD); }
+void jvm_emitter::emit_iadd() { bytecode.push_back(OP_IADD); }
 
-void jvm_emitter::emit_isub() { bytecode_.push_back(OP_ISUB); }
+void jvm_emitter::emit_isub() { bytecode.push_back(OP_ISUB); }
 
-void jvm_emitter::emit_imul() { bytecode_.push_back(OP_IMUL); }
+void jvm_emitter::emit_imul() { bytecode.push_back(OP_IMUL); }
 
-void jvm_emitter::emit_idiv() { bytecode_.push_back(OP_IDIV); }
+void jvm_emitter::emit_idiv() { bytecode.push_back(OP_IDIV); }
 
 void jvm_emitter::emit_iload(uint8_t idx) {
-  bytecode_.push_back(OP_ILOAD);
-  bytecode_.push_back(idx);
+  bytecode.push_back(OP_ILOAD);
+  bytecode.push_back(idx);
 }
 
 void jvm_emitter::emit_istore(uint8_t idx) {
-  bytecode_.push_back(OP_ISTORE);
-  bytecode_.push_back(idx);
+  bytecode.push_back(OP_ISTORE);
+  bytecode.push_back(idx);
 }
 
 void jvm_emitter::trace() const {
-  for (auto byte : bytecode_) {
+  for (auto byte : bytecode) {
     printf("%02X ", byte);
   }
 
   std::cout << std::endl;
+}
+
+// @todo: emit based on subpath/dirname of input
+void write_class(const std::string& filename,
+                 const std::vector<uint8_t>& bytecode) {
+  std::ofstream file(filename, std::ios::binary);
+
+  if (!file.is_open()) {
+    throw std::runtime_error("failed to open file (for write): " + filename);
+  }
+
+  file.write(reinterpret_cast<const char*>(&JAVA_CLASS_MAGIC_HEADER),
+             sizeof(JAVA_CLASS_MAGIC_HEADER));
+  file.write(reinterpret_cast<const char*>(bytecode.data()), bytecode.size());
+
+  file.close();
 }
 
 void __test_emit__() {
@@ -87,4 +103,5 @@ void __test_emit__() {
   std::cout << "--------------------------" << std::endl;
 
   emitter.trace();
+  write_class("./tests/main.class", emitter.bytecode);
 }
--- src/emit.h
+++ src/emit.h
@@ -4,33 +4,46 @@
 #define EMIT_H
 
 #include <cstdint>
+#include <string>
 #include <vector>
 
+// clang-format off
+
+const uint32_t JAVA_CLASS_MAGIC_HEADER = 0xCAFEBABE; // 4-byte header
+const uint16_t JAVA_CLASS_MINOR_VERSION = 0x0000;    // minor version 0
+const uint16_t JAVA_CLASS_MAJOR_VERSION = 0x0034;    // major version 52 (Java SE 8)
+
 // https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html
-const uint8_t OP_NOP = 0x00;
-const uint8_t OP_ICONST_0 = 0x03;
-const uint8_t OP_ICONST_1 = 0x04;
-const uint8_t OP_ICONST_2 = 0x05;
-const uint8_t OP_ICONST_3 = 0x06;
-const uint8_t OP_ICONST_4 = 0x07;
-const uint8_t OP_ICONST_5 = 0x08;
-const uint8_t OP_BIPUSH = 0x10;
-const uint8_t OP_SIPUSH = 0x11;
-const uint8_t OP_LDC = 0x12;
-const uint8_t OP_ILOAD = 0x15;
-const uint8_t OP_ISTORE = 0x36;
-const uint8_t OP_IADD = 0x60;
-const uint8_t OP_ISUB = 0x64;
-const uint8_t OP_IMUL = 0x68;
-const uint8_t OP_IDIV = 0x6C;
-const uint8_t OP_IRETURN = 0xAC;
-const uint8_t OP_RETURN = 0xB1;
+const uint8_t OP_NOP =          0x00;
+const uint8_t OP_ICONST_0 =     0x03;
+const uint8_t OP_ICONST_1 =     0x04;
+const uint8_t OP_ICONST_2 =     0x05;
+const uint8_t OP_ICONST_3 =     0x06;
+const uint8_t OP_ICONST_4 =     0x07;
+const uint8_t OP_ICONST_5 =     0x08;
+const uint8_t OP_BIPUSH =       0x10;
+const uint8_t OP_SIPUSH =       0x11;
+const uint8_t OP_LDC =          0x12;
+const uint8_t OP_ILOAD =        0x15;
+const uint8_t OP_ISTORE =       0x36;
+const uint8_t OP_IADD =         0x60;
+const uint8_t OP_ISUB =         0x64;
+const uint8_t OP_IMUL =         0x68;
+const uint8_t OP_IDIV =         0x6C;
+const uint8_t OP_IRETURN =      0xAC;
+const uint8_t OP_RETURN =       0xB1;
 
 // only used to test against fixtures
 void __test_emit__();
 
+void write_class(const std::string& filename,
+                 const std::vector<uint8_t>& bytecode);
+
+// clang-format on
+
 class jvm_emitter {
  public:
+  std::vector<uint8_t> bytecode;
   jvm_emitter();
 
   // we need a less-redundant way of emitting without
@@ -47,9 +60,6 @@ class jvm_emitter {
   void emit_iload(uint8_t index);
   void emit_istore(uint8_t index);
   void trace() const;
-
- private:
-  std::vector<uint8_t> bytecode_;
 };
 
 #endif  // EMIT_H
